561310f8aa7738ab66f38ed7bf0d2c4cb978c86c
Javier Villca <javieranibalvillca@gmail.com>
2024-02-02 00:59:41 -0300

    creando las carpetas de los modulos de la aplicacion

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
+/* eslint-disable no-console */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const configService = app.get(ConfigService);
+  const PORT = configService.get<number>('SERVER_PORT');
+  const HOST = configService.get<string>('SERVER_HOST');
+
+  await app.listen(PORT, HOST);
+  console.log(`http://${HOST}:${PORT}`);
 }
 bootstrap();
